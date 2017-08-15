d5af5d8562b075030d1df1ae92302c1f022bf450
rdzur <rdzur@bhinc.com>
2017-08-15 16:09:00 -0400

    Update decode.js

---
+++ 00-workspace/decode.js
@@ -0,0 +1,13 @@
+window.polyline = {};
+window.polyline.decode = function(str, precision) {
+    var index = 0,
+        lat = 0,
+        lng = 0,
+        coordinates = [],
+        shift = 0,
+        result = 0,
+        byte = null,
+        latitude_change,
+        longitude_change,
+        factor = Math.pow(10, precision || 6);
+
